2b2464b7cd200d943f2b3af9ef9747aa169978b4
SHUNPAN LO <shunpanlo@SHUNPANde-MacBook-Air.local>
2018-02-21 20:57:02 +0800

    finish swoosh

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by SHUNPAN LO on 21/2/2018.
+//  Copyright © 2018年 bensonlspapp. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selctedSkillLevel: String!
+}
